a20ee4e47c1002102584a98669bfa341cfa123db
phantie <funtom1605@gmail.com>
2021-01-04 10:58:47 +0200

    inheritence instead of proxying

--- bmap/__init__.py
+++ bmap/__init__.py
@@ -1,9 +1,9 @@
 from collections import OrderedDict
 
 __all__ = ('BoundSizedDict',)
-__version__ = '0.1'
+__version__ = '0.2'
 
-class BoundSizedDict:
+class BoundSizedDict(OrderedDict):
     def __new__(cls, max):
         if isinstance(max, int) and max >= 1:
             return super().__new__(cls)
@@ -11,24 +11,23 @@ class BoundSizedDict:
             raise ValueError('max must be an instance of int and >= 1')
 
     def __init__(self, max):        
-        self._ = OrderedDict()
         self.max = max
 
-    def __getitem__(self, item):
-        return self._.__getitem__(item)
-
     def __setitem__(self, key, value):
-        _ = self._
-        _.__setitem__(key, value)
-        _.move_to_end(key)
-        if len(_) > self.max:
-            _.popitem(last=False)
-
-    def __delitem__(self, item):
-        self._.__delitem__(item)
+        super().__setitem__(key, value)
+        self.move_to_end(key)
+        if len(self) > self.max:
+            self.popitem(last=False)
 
     def __str__(self):
-        return '{' + ', '.join(f'{k!r}: {v!r}' for k, v in self._.items()) + '}'
+        return '{' + ', '.join(f'{k!r}: {v!r}' for k, v in self.items()) + '}'
 
     def __repr__(self):
         return str(self)
+
+    @classmethod
+    def fromkeys(cls, keys):
+        raise DeprecationWarning('nondetermenistic behaviour will be achieved using this method')
+
+    def update(self, dict):
+        raise DeprecationWarning('nondetermenistic behaviour will be achieved using this method')
